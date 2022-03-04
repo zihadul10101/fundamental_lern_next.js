4f60dfb3432936920237b3c3d9c833722732bf47
Zihadul Islam <zihadul10101@gmail.com>
2022-03-04 16:10:26 +0600

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 const Home = () => {
+  const router = useRouter()
+  const handleonClick = () => {
+    console.log("placing your order");
+    router.push('/product')
+  }
   return (
     <div>
       <h1>Home page</h1>
@@ -11,6 +17,7 @@ const Home = () => {
       <Link href="/product">
         <a>Product</a>
       </Link>
+      <button onClick={handleonClick}>Please Order</button>
     </div>
   );
 };
